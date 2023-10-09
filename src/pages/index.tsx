8090fab9a3a0aca06d04f0aade6c54dd42506b0e
Dmarcellakbar <dickymarcellino@gmail.com>
2023-10-09 11:16:51 +0700

    update 7

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,14 +62,14 @@ export default function Home() {
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   <div className="px-6 py-6 lg:px-8">
-                    <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                    <h3 className="mb-4 text-xl font-medium text-gray-900 ">
                       Contact Me
                     </h3>
                     <form className="space-y-6" action="#">
                       <div>
                         <label
                           htmlFor="name"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 "
                         >
                           Name
                         </label>
@@ -77,7 +77,7 @@ export default function Home() {
                           type="name"
                           name="name"
                           id="name"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                           placeholder="Type your name here"
                           required
                         />
@@ -85,14 +85,14 @@ export default function Home() {
                       <div>
                         <label
                           htmlFor="phone"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 "
                         >
                           Whatsapp
                         </label>
                         <input
                           type="tel"
                           id="phone"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                           placeholder="Example: 628123456789"
                           pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
                           required
@@ -101,14 +101,14 @@ export default function Home() {
                       <div>
                         <label
                           htmlFor="countries"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 "
                         >
                           Product
                         </label>
                         <select
                           placeholder="Choose the desired product"
                           id="product"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                         >
                           <option value="website">Website</option>
                           <option value="mobile">Mobile Apps</option>
@@ -118,14 +118,14 @@ export default function Home() {
                       <div>
                         <label
                           htmlFor="message"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 "
                         >
                           Concept
                         </label>
                         <textarea
                           id="message"
                           rows={4}
-                          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
                           defaultValue={''}
                         />
                       </div>
--- src/pages/website/index.tsx
+++ src/pages/website/index.tsx
@@ -41,14 +41,14 @@ export default function Website() {
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   <div className="px-6 py-6 lg:px-8">
-                    <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                    <h3 className="mb-4 text-xl font-medium text-gray-900 ">
                       Contact Me
                     </h3>
                     <form className="space-y-6" action="#">
                       <div>
                         <label
                           htmlFor="name"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 "
                         >
                           Name
                         </label>
@@ -56,7 +56,7 @@ export default function Website() {
                           type="name"
                           name="name"
                           id="name"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                           placeholder="Type your name here"
                           required
                         />
@@ -64,14 +64,14 @@ export default function Website() {
                       <div>
                         <label
                           htmlFor="phone"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 "
                         >
                           Whatsapp
                         </label>
                         <input
                           type="tel"
                           id="phone"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                           placeholder="Example: 628123456789"
                           pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
                           required
@@ -80,14 +80,14 @@ export default function Website() {
                       <div>
                         <label
                           htmlFor="countries"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 "
                         >
                           Product
                         </label>
                         <select
                           placeholder="Choose the desired product"
                           id="product"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                         >
                           <option value="website">Website</option>
                           <option value="mobile">Mobile Apps</option>
@@ -97,14 +97,14 @@ export default function Website() {
                       <div>
                         <label
                           htmlFor="message"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 "
                         >
                           Concept
                         </label>
                         <textarea
                           id="message"
                           rows={4}
-                          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
                           defaultValue={''}
                         />
                       </div>
